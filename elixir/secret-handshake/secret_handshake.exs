338f7d85a0eca9864882ab9a32f34a099ee0f05a
Rafael Santos <rafael.santos@estrategiaconcursos.com.br>
2019-07-02 13:54:40 -0300

    Secret Handshake solution

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -13,14 +13,26 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+  require Bitwise
+
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    steps = [
-        {1: "wink"},
-        {2: "double blink"},
-        {4: "close your eyes"},
-        {8: "jump"},
-    ]
-     Integer.to_string(19, 2)
+    list =
+      [{1, "wink"}, {2, "double blink"}, {4, "close your eyes"}, {8, "jump"}]
+      |> Enum.reduce([], fn val, acc ->
+        cond do
+          Bitwise.band(code, elem(val, 0)) > 0 ->
+            acc ++ [elem(val, 1)]
+
+          true ->
+            acc
+        end
+      end)
+
+    if Bitwise.band(code, 0b10000) > 0 do
+      Enum.reverse(list)
+    else
+      list
+    end
   end
 end
