2d6b3cee0a45c3a445ade2a677595a4787d916a4
Rafael Santos <rafael.santos@estrategiaconcursos.com.br>
2019-08-08 18:47:07 -0300

    Solution for strain exercise without tail-call optimization

--- elixir/strain/lib/strain.ex
+++ elixir/strain/lib/strain.ex
@@ -6,8 +6,18 @@ defmodule Strain do
   Do not use `Enum.filter`.
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, fun) do
+  def keep(list, fun) when length(list) > 0 do
+    [item | newList] = list
+
+    cond do
+      fun.(item) ->
+        [item] ++ keep(newList, fun)
+      true ->
+        keep(newList, fun)
+    end
   end
+
+  def keep([], _), do: []
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
@@ -16,6 +26,16 @@ defmodule Strain do
   Do not use `Enum.reject`.
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def discard(list, fun) do
+  def discard(list, fun) when length(list) > 0 do
+    [item | newList] = list
+
+    cond do
+      fun.(item) ->
+        discard(newList, fun)
+      true ->
+        [item] ++ discard(newList, fun)
+    end
   end
+
+  def discard([], _), do: []
 end
